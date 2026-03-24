5238e04b67af081f3082fa84603981807626b7a5
mardozok <mardozok@gmail.com>
2026-03-24 05:29:16 -0300

    add new code

---
+++ code/index.js
@@ -0,0 +1,39 @@
+const API_URL = "https://invadable-chasidy-fleshly.ngrok-free.dev/products";
+
+function log(msg) {
+    document.getElementById("log").innerText += msg + "\n";
+}
+
+function startScanner() {
+    const html5QrCode = new Html5Qrcode("reader");
+
+    Html5Qrcode.getCameras().then(devices => {
+        if (devices && devices.length) {
+            let cameraId = devices.find(device =>
+                device.label.toLowerCase().includes('back') ||
+                device.label.toLowerCase().includes('rear')
+            )?.id || devices[0].id;
+
+            html5QrCode.start(
+                { facingMode: "environment" },
+                { fps: 10, qrbox: 250 },
+                (decodedText) => {
+                    log("SCAN: " + decodedText);
+
+                    fetch(`${API_URL}/${decodedText}`, {
+                        method: "POST"
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            log("RESPUESTA: " + JSON.stringify(data));
+                        })
+                        .catch(err => log("ERROR: " + err));
+
+                    html5QrCode.stop();
+                }
+            );
+        }
+    }).catch(err => {
+        log("ERROR CAMARA: " + err);
+    });
+}
